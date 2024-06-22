f60eab3e83a2a4e77b72ef7253c4a801c1bbf92d
nitintandukar <nitin.tandukar0422@gmail.com>
2024-06-22 20:28:55 +0545

    Added a section in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,8 +33,9 @@ const HomePage = () => {
                 Exploring ideas that matter.
               </p>
               <Button
+                size={"lg"}
                 variant={"outline"}
-                className="animate-bounce border-2 w-2/7 border-neutral-600 rounded-xl  hover:shadow-xl hover:shadow-neutral-700"
+                className="animate-bounce  border-2  border-neutral-600 rounded-xl  hover:shadow-xl hover:shadow-neutral-700"
                 onClick={() => navigate("/blogs")}
               >
                 {" "}
@@ -48,47 +49,66 @@ const HomePage = () => {
 
       <h1 className="text-center font-light text-3xl md:text-5xl pt-9">
         {" "}
-        Embark on a journey of discovery and inspiration 
+        Embark on a journey of discovery and inspiration
       </h1>
       <div className=" flex justify-center">
-        <div className=" w-5/6 sm:w-3/5 flex flex-col lg:flex-row justify-center items-center gap-3">
-          <Card className="lg:w-3/4 shadow-xl">
+        <div className=" w-5/6 lg:w-3/5 flex flex-col md:flex-row justify-center items-center gap-9">
+          <Card className="md:min-h-full md:w-3/4 shadow-xl">
             <div className=" flex justify-center p-5 pb-2 ">
-                <img src="../src/assets/banner.jpg" className=" w-full h-56 rounded-md shadow-md"/>
+              <img
+                src="../src/assets/banner.jpg"
+                className=" w-full h-56 rounded-md shadow-md"
+              />
             </div>
-            
-            <p className="mx-5 font-bold text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            
-            <CardContent>
-            
+
+            <p className="mx-5 font-bold text-lg">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Cupiditate quod fuga laudantium itaque amet. Saepe vel minus
-              accusamus perferendis, quos dolorem atque, quisquam eum fugit
-              asperiores facilis eaque explicabo, blanditiis quod tempore
-              tenetur. Vero, quibusdam?
+            </p>
+
+            <CardContent className="text-justify text-sm">
+              <span className=" text-neutral-500">By </span>
+              <span className=" font-medium">Nitin Tandukar </span>
+              <span className="text-yellow-700">on April 28 2023 </span>
             </CardContent>
           </Card>
-          <Card className="lg:w-3/4 shadow-xl">
+          <Card className="md:min-h-full md:w-3/4 shadow-xl">
             <div className=" flex justify-center p-5 pb-2 ">
-                <img src="../src/assets/banner3.jpg" className=" w-full h-56 rounded-md shadow-md"/>
+              <img
+                src="../src/assets/banner3.jpg"
+                className=" w-full h-56 rounded-md shadow-md"
+              />
             </div>
-            
-            <p className="mx-5 font-bold text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            
-            <CardContent>
-            
+
+            <p className="mx-5 font-bold text-lg">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Cupiditate quod fuga laudantium itaque amet. Saepe vel minus
-              accusamus perferendis, quos dolorem atque, quisquam eum fugit
-              asperiores facilis eaque explicabo, blanditiis quod tempore
-              tenetur. Vero, quibusdam?
+            </p>
+
+            <CardContent className="text-justify text-sm">
+              <span className=" text-neutral-500">By </span>
+              <span className=" font-medium">Nitin Tandukar </span>
+              <span className="text-yellow-700">on April 28 2023 </span>
             </CardContent>
           </Card>
-          
         </div>
       </div>
-      <div>Some advertisement of the website</div>
-      <div>A button to start reading</div>
+      <div className="bg-red-900">
+        <h1> Vertex FAQ's </h1>
+      </div>
+      <div className="">
+        <div className="bg-[url('src/assets/world2.jpg')] h-[350px] sm:h-[500px]  bg-center bg-cover bg-no-repeat">
+          <div className="h-full  bg-black  bg-opacity-60 flex flex-col justify-center items-center gap-5 text-neutral-200">
+            <h1 className=" text-xl font-medium md:text-5xl"> Join millions of others</h1>
+            <p className="text-center text-sm font-thin px-9 sm:px-16 lg:px-96 md:text-xl">
+              Whether sharing your expertise, breaking news, or whatever’s on
+              your mind, you’re in good company on Vertex. Sign up to discover
+              why millions of people have published their passions here.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-neutral-400 h-96 text-center">
+        Some sort of footer options
+      </div>
     </div>
   );
 };
