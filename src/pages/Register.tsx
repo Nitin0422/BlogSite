3e2ab5d51d6c3feea27a4a0a7c54e608ac5a356e
nitintandukar <nitin.tandukar0422@gmail.com>
2024-06-24 16:42:34 +0545

    Configured react hook form, zod and resolvers

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,18 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 const Register = () => {
     return (
-        <div>
-            THis will be register page
+        <div className="flex bg-slate-00  h-screen justify-center items-center">
+            <Card className=" h-1/3 w-1/2">
+                <CardHeader>
+                    <CardTitle className="text-center">
+                        VERTEX <span className="text-neutral-500 font-medium">| Registration Form</span> 
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    
+                </CardContent>
+            </Card>
         </div>
     )
 }
