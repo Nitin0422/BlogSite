9536b38f6817456ac7e3fba311d9ec365acd17fc
nitintandukar <nitin.tandukar0422@gmail.com>
2024-08-28 12:47:05 +0545

    Removed timeout from axios client

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:8000/",
-  timeout: 1000,
 });
 api.defaults.xsrfCookieName = "csrftoken";
 api.defaults.xsrfHeaderName = "X-CSRFToken";
