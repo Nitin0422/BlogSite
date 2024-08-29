dda51138ccf82f8a3ef301f1277347da96d4fcae
nitintandukar <nitin.tandukar0422@gmail.com>
2024-08-29 11:07:12 +0545

    Integrated APIs

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,3 +1,4 @@
+import Loader from "@/components/Loader";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
@@ -9,9 +10,12 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import api from "@/utils/api";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { toast, Toaster } from "sonner";
 import { z } from "zod";
 
 const formSchema = z.object({
@@ -19,7 +23,8 @@ const formSchema = z.object({
 });
 
 const ForgotPassword = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -28,8 +33,26 @@ const ForgotPassword = () => {
   });
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
-    } catch (error) {}
+      setLoading(true);
+      const { data, status } = await api.post("api/user/send/password/reset/email/", values);
+      toast.success(`${data.message} Redirecting you to login page...`)
+      setTimeout(() => {
+        navigate('/login');
+      }, 3000);  
+      
+      if (status === 400) {
+        toast.error(data.errors.non_field_errors[0]);
+      } else {
+        console.log(data);
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+      toast.error("Something went wrong, please try again later.");
+    } finally {
+      setLoading(false);
+    }
   }
+  
   return (
     <div className="flex h-screen justify-center items-center">
       <Card className="3/4 md:w-1/2 lg:w-1/3 ">
@@ -60,8 +83,8 @@ const ForgotPassword = () => {
                   </FormItem>
                 )}
               />
-              <Button type="submit" className="mt-2">
-                Send Password Reset Link
+               <Button type="submit" className="mt-2">
+                {loading ? <Loader /> : <>Send Password Reset Link</>}
               </Button>
               <p className="text-center text-sm font-light">
                 {" "}
@@ -72,12 +95,12 @@ const ForgotPassword = () => {
                 >
                   Login.
                 </span>
-                
               </p>
             </form>
           </Form>
         </CardContent>
       </Card>
+      <Toaster richColors position="top-right" closeButton />
     </div>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -41,7 +41,14 @@ const Login = () => {
   const navigate = useNavigate();
   const { setToken } = useAuth();
   const [loading, setLoading] = useState<boolean>(false);
-
+  const query = new URLSearchParams(window.location.search);
+  const resetPwdEmailSent = query.get("resetPwdEmailSent");
+  if (resetPwdEmailSent === "True") {
+    toast.success(
+      "Password reset email has been sent to your email. Follow the link to reset your password!",
+      { duration: 10000 }
+    );
+  }
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -111,7 +118,7 @@ const Login = () => {
               />
 
               <Button type="submit" className="mt-2">
-                {loading ? <Loader/> : <>Login</>}
+                {loading ? <Loader /> : <>Login</>}
               </Button>
               <p className="text-center text-sm font-light">
                 {" "}
