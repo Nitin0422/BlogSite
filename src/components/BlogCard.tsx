d369ef2bdcd9d206faa453c3135829a0fac773da
nitintandukar <nitin.tandukar0422@gmail.com>
2024-09-05 18:30:39 +0545

    Added separator and enhanced some designs

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,17 +1,7 @@
 import { motion } from "framer-motion";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { MessageCircle, Sparkles, ThumbsDown, ThumbsUp } from "lucide-react";
-
-interface BlogCardProps {
-  userName: string;
-  blogTitle: string;
-  blogDesc: string;
-  blogPostDate: string;
-  likes: number;
-  dislikes: number;
-  comments: number;
-  image: string;
-}
+import { Separator } from "./ui/separator";
 
 const BlogCard = ({
   userName,
@@ -22,7 +12,7 @@ const BlogCard = ({
   dislikes,
   comments,
   image,
-}: BlogCardProps) => {
+}: Blog) => {
   const getInitials = (name: string) => {
     const nameParts = name.split(" ");
     const initials = nameParts.map((part) => part[0].toUpperCase());
@@ -31,11 +21,10 @@ const BlogCard = ({
 
   return (
     <motion.div
-      className="grid grid-cols-3 p-1 lg:w-5/6 border-b-2"
+      className="grid grid-cols-3 lg:w-5/6 shadow-neutral-300 shadow-sm rounded-md p-3"
       initial={{ opacity: 0.2 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-    //   viewport={{ once: true }}
     >
       <div className=" col-span-2  p-1 grid grid-rows-5 gap-3 max-h-48">
         <div className="row-span-1 flex gap-2 items-center">
@@ -78,23 +67,6 @@ const BlogCard = ({
       <div className="col-span-1  p-1 overflow-hidden max-h-48">
         <img src={image} className="w-full h-full object-cover" />
       </div>
-
-      {/* <div className=" flex justify-center p-5 pb-2 ">
-        <img
-          src="../src/assets/banner3.jpg"
-          className=" w-full h-56 rounded-md shadow-md"
-        />
-      </div>
-
-      <p className="mx-5 font-bold text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
-
-      <CardContent className="text-justify text-sm">
-        <span className=" text-neutral-500">By </span>
-        <span className=" font-medium">Nitin Tandukar </span>
-        <span className="text-yellow-700">on April 28 2023 </span>
-      </CardContent> */}
     </motion.div>
   );
 };
